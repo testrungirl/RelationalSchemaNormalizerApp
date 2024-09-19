9a9ae41ad965e12319e7195d9c6d6378ba7c1789
testrungirl <laigbodioh@gmail.com>
2024-09-19 11:08:24 +0100

    Model update

---
+++ RelationalSchemaNormalizerUI/Migrations/20240919100723_allowEmptyValuesForImages.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationalSchemaNormalizerUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class allowEmptyValuesForImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgPathFor3NF",
+                table: "TableDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgPathFor2NF",
+                table: "TableDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgPathFor3NF",
+                table: "TableDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgPathFor2NF",
+                table: "TableDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "");
+        }
+    }
+}
